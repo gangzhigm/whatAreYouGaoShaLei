12247f5920a803e7e2aefb058ad82e03498267c0
gangzhigm <864853934@qq.com>
2018-03-08 18:29:58 +0800

    WOW

---
+++ test/test-typescript/class.ts
@@ -0,0 +1,16 @@
+class Student{
+    fullName:string;
+    constructor(public firstName,public middleInitial,public lastName){
+        this.fullName=firstName+""+middleInitial+""+lastName;
+    }
+}
+interface Person{
+    firstName:string;
+    lastName:string;
+}
+function greeter(person:Person){
+    return "hello,"+person.firstName+""+person.lastName;
+}
+var user =new Student("six","6.","user");
+document.body.innerHTML=greeter(user);
+console.log(user);
---
+++ test/test-weex/awesome-app/src/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <text>Hello World</text>
+  </div>
+</template>
+<style>
+</style>
+<script>
+  export default {
+  }
+</script>
