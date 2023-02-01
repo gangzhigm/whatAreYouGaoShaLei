64527b407a30429e58393331bfd834dddf01db1b
gangzhigm <864853934@qq.com>
2023-02-01 18:28:35 +0800

    update psnDB front

--- packages/psnDB/front/src/components/HelloWorld.vue
+++ packages/psnDB/front/src/components/HelloWorld.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: gangzhigm 864853934@qq.com
+ * @Date: 2023-02-01 18:18:46
+ * @LastEditors: gangzhigm 864853934@qq.com
+ * @LastEditTime: 2023-02-01 18:27:24
+ * @FilePath: \whatAreYouGaoShaLei\packages\psnDB\front\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
 import { ref } from 'vue'
 
@@ -7,28 +15,13 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <input placeholder="cookie" type="text">
+    <button>获取用户信息</button>
+    <button>获取用户游戏列表</button>
+    <input placeholder="游戏ID" type="text">
+    <button>获取游戏详细内容</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
